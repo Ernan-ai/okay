30134ad2f316e40c2d06fa8d3e6119014f617059
eklessiia <eklessiia@eklessiias-Mac.local>
2025-06-21 01:29:24 +0600

    Header animation type shi

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Header.css';
 import logo from '../Logo/logo-white.jpg';
 
 const Header = ({ scrollY }) => {
- const halfwayPoint = window.innerHeight / 2;
- const isHidden = scrollY > halfwayPoint;
- 
- return (
-   <nav 
-     className={`header ${isHidden ? 'header-hidden' : ''}`}
-   >
-     <div className="logo">
-       <img src={logo} alt="logo" className='logo'/>
-     </div>
-     <button className="menu-button">
-       <div className="menu-line"></div>
-       <div className="menu-line"></div>
-       <div className="menu-line"></div>
-     </button>
-   </nav>
- );
+  const [isHidden, setIsHidden] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      
+      if (currentScrollY > lastScrollY && currentScrollY > 100) {
+        setIsHidden(true);
+      } else {
+        setIsHidden(false);
+      }
+      
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [lastScrollY]);
+
+  return (
+    <nav className={`header ${isHidden ? 'header-hidden' : ''}`}>
+      <div className="logo">
+        <img src={logo} alt="logo" className='logo'/>
+      </div>
+      <button className="menu-button">
+        <div className="menu-line"></div>
+        <div className="menu-line"></div>
+        <div className="menu-line"></div>
+      </button>
+    </nav>
+  );
 };
 
 export default Header;
